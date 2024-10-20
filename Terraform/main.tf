15075e720f0e77e05551379888119584854bdb2d
Huong Tran <tranthithuhuong0510@gmail.com>
2024-10-20 11:37:49 +0700

    remove restrictions

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -46,25 +46,25 @@ resource "azurerm_network_security_rule" "AllowInbound" {
   protocol                    = "*"
   source_port_range           = "*"
   destination_port_range      = "80"
-  source_address_prefix       = "VirtualNetwork"
+  source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.main.name
   network_security_group_name = azurerm_network_security_group.main.name
 }
 
-resource "azurerm_network_security_rule" "AllowInbound-LB" {
-  name                        = "${var.prefix}-AllowInbound-LB"
-  priority                    = 150
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "AzureLoadBalancer"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.main.name
-  network_security_group_name = azurerm_network_security_group.main.name
-}
+# resource "azurerm_network_security_rule" "AllowInbound-LB" {
+#   name                        = "${var.prefix}-AllowInbound-LB"
+#   priority                    = 150
+#   direction                   = "Inbound"
+#   access                      = "Allow"
+#   protocol                    = "*"
+#   source_port_range           = "*"
+#   destination_port_range      = "*"
+#   source_address_prefix       = "AzureLoadBalancer"
+#   destination_address_prefix  = "*"
+#   resource_group_name         = azurerm_resource_group.main.name
+#   network_security_group_name = azurerm_network_security_group.main.name
+# }
 
 # resource "azurerm_network_security_rule" "DenyFromInternet" {
 #   name                        = "${var.prefix}-DenyFromInternet"
